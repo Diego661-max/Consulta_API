43949590b0c8fa728fa8c47c6b88be354c39bd14
Diego Jesus Santos <00001123455090sp@al.educacao.sp.gov.br>
2026-06-18 23:24:29 -0300

    Parte Final

--- lib/services/omdb_service.dart
+++ lib/services/omdb_service.dart
@@ -2,37 +2,45 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/filme.dart';
 
-class OmdbService{
+class OmdbService {
 
+  //final String apiKey = 'SUA_CHAVE';
   final String apiKey = '7ce47bdf';
+
 
   Future<List<Filme>> buscarFilmes(String nomeFilme) async {
 
     List<Filme> listaFilmes = [];
 
-    final urlBusca = 'http://www.omdbapi.com/?i=$nomeFilme&apikey=$apiKey';
+    final urlBusca =
+        'https://www.omdbapi.com/?s=$nomeFilme&apikey=$apiKey';
 
-    final responseBusca = await http.get(Uri.parse(urlBusca));
+    final responseBusca =
+        await http.get(Uri.parse(urlBusca));
 
-     final dadosBusca = json.decode(responseBusca.body);
+    final dadosBusca =
+        json.decode(responseBusca.body);
 
-     if(dadosBusca['Search'] != null){
+    if (dadosBusca['Search'] != null) {
 
-      for(var item in dadosBusca['Search']){
-         
-        final imdbID = item['imdID'];
-        
-        final urlDetalhes = ' http://www.omdbapi.com/?i=$imdbID&plot=short&apikey=$apiKey';
+      for (var item in dadosBusca['Search']) {
 
-        final responseDetalhes = await http.get(Uri.parse(urlDetalhes));
+        final imdbID = item['imdbID'];
 
-        final dadosDetalhes = json.decode(responseDetalhes.body);
+        final urlDetalhes =
+            'https://www.omdbapi.com/?i=$imdbID&plot=short&apikey=$apiKey';
+
+        final responseDetalhes =
+            await http.get(Uri.parse(urlDetalhes));
+
+        final dadosDetalhes =
+            json.decode(responseDetalhes.body);
 
         listaFilmes.add(
           Filme.fromJson(dadosDetalhes),
         );
       }
-     }
+    }
     return listaFilmes;
   }
 }
